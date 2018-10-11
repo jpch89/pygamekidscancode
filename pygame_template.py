f480b38d060d3356d3630839dcb6d5c95b4c324f
进击的团子 <jpch89@outlook.com>
2018-10-11 22:15:10 +0800

    01. Getting started with Pygame

---
+++ pygame_template.py
@@ -0,0 +1,39 @@
+# Pygame template - skeleton for a new game project
+import pygame
+import random
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('我的游戏')
+clock = pygame.time.Clock()
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+    # Update
+    # Draw / Render
+    screen.fill(BLACK)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
