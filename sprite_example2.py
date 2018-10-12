c1bc15479b60651de857d4477137c2f7916b40c9
进击的团子 <jpch89@outlook.com>
2018-10-12 10:01:47 +0800

    More about Sprites

---
+++ sprite_example2.py
@@ -0,0 +1,79 @@
+# Pygame template - skeleton for a new game project
+import pygame
+import random
+import os
+
+WIDTH = 800
+HEIGHT = 600
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# set up assets folders
+# Windows: "C:\Users\jpch89\Documents"
+# Mac: "/Users/jpch89/Documents"
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'img')
+
+
+class Player(pygame.sprite.Sprite):
+    # sprite for the player
+    def __init__(self):
+        # pygame.sprite.Sprite.__init__(self)
+        super().__init__()
+        # self.image = pygame.Surface((50, 50))
+        # self.image.fill(GREEN)
+        # self.image = pygame.image.load(os.path.join(img_folder, 'p1_jump.png')).convert()
+        # self.image.set_colorkey(BLACK)
+        # 或者直接使用官网推荐的 convert_alpha()
+        self.image = pygame.image.load(os.path.join(img_folder, 'p1_jump.png')).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.y_speed = 5
+
+    def update(self):
+        self.rect.x += 5
+        self.rect.y += self.y_speed
+        if self.rect.bottom > HEIGHT - 200:
+            self.y_speed = -5
+        if self.rect.top < 200:
+            self.y_speed = 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('我的游戏')
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+    # Update
+    all_sprites.update()
+    # Draw / Render
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
