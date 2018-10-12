f419cb45a64cf55aa1c90de03b9b4299aff43098
进击的团子 <jpch89@outlook.com>
2018-10-12 12:15:03 +0800

    05. Platformer-02-Player Movement

--- platform/settings.py
+++ platform/settings.py
@@ -1,8 +1,8 @@
 # game options/settings
-TITLE = '我的游戏'
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+TITLE = '跳跳'
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
 
 # define colors
 WHITE = (255, 255, 255)
@@ -10,3 +10,8 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
