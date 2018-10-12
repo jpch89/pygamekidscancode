ee8b4a2e2c7d9d1a3f5789d673f68646e20cbae3
进击的团子 <jpch89@outlook.com>
2018-10-12 10:52:39 +0800

    04. Platformer-01-Setting up the Project

---
+++ platform/settings.py
@@ -0,0 +1,12 @@
+# game options/settings
+TITLE = '我的游戏'
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
