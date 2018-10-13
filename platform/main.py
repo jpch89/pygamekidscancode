03e3c98a7822157020265edcabbc86726b9d3ed8
进击的团子 <jpch89@outlook.com>
2018-10-13 09:39:26 +0800

    09. Platformer-06-Game Over

--- platform/main.py
+++ platform/main.py
@@ -14,9 +14,11 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
 
     def new(self):
         # start a new game
+        self.score = 0
         self.all_sprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
         self.player = Player(self)
@@ -74,6 +76,17 @@ class Game:
                 plat.rect.y += abs(self.player.vel.y)
                 if plat.rect.top >= HEIGHT:
                     plat.kill()
+                    self.score += 10
+
+        # Die!
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+        if len(self.platforms) == 0:
+            self.playing = False
+
 
         # spawn new platforms to keep same average number
         while len(self.platforms) < 6:
@@ -89,6 +102,7 @@ class Game:
         # Game loop - draw
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
+        self.draw_text(str(self.score), 22, WHITE, WIDTH/2, 15)
         # *after* drawing everything, flip the display
         pg.display.flip()
 
@@ -99,6 +113,13 @@ class Game:
     def show_go_screen(self):
         # game over/continue
         pass
+
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
 
 
 g = Game()
